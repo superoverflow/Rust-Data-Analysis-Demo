8a915ad08f5368e9967b0ce241503c27522e6894
Cyrus Tang <cyrus.tang@superoverflow.com>
2021-11-10 21:29:59 +0000

    getting MACD

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::fs::File;
 use std::io::prelude::Read;
 use std::io::Cursor;
 use std::path::Path;
+use std::iter::Iterator;
+
+use yata::core::Candle;
+use yata::helpers::RegularMethods;
+use yata::indicators::MACD;
+use yata::prelude::*;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 async fn fetch_binance_kline_data(
@@ -36,31 +42,29 @@ fn unpack_binance_kline_data(file_name: &str) -> String {
 struct BinanceKline {
     start_time: i64,
     end_time: i64,
-    open: f32,
-    close: f32,
-    high: f32,
-    low: f32,
-    volume: f32,
+    open: f64,
+    close: f64,
+    high: f64,
+    low: f64,
+    volume: f64,
 }
 
-fn transform_kline_data(data: String) -> Vec<BinanceKline> {
+fn transform_kline_data(data: String) -> Vec<Candle> {
     let lines = data.split("\n");
-    let mut result: Vec<BinanceKline> = Vec::new();
+    let mut result: Vec<Candle> = Vec::new();
     for line in lines {
         if !line.contains(",") {
             continue;
         }
         let mut data = line.split(",");
         let start_time: i64 = data.next().unwrap().parse().unwrap();
-        let open: f32 = data.next().unwrap().parse().unwrap();
-        let close: f32 = data.next().unwrap().parse().unwrap();
-        let high: f32 = data.next().unwrap().parse().unwrap();
-        let low: f32 = data.next().unwrap().parse().unwrap();
-        let volume: f32 = data.next().unwrap().parse().unwrap();
+        let open: f64 = data.next().unwrap().parse().unwrap();
+        let close: f64 = data.next().unwrap().parse().unwrap();
+        let high: f64 = data.next().unwrap().parse().unwrap();
+        let low: f64 = data.next().unwrap().parse().unwrap();
+        let volume: f64 = data.next().unwrap().parse().unwrap();
         let end_time: i64 = data.next().unwrap().parse().unwrap();
-        let parsed = BinanceKline {
-            start_time,
-            end_time,
+        let parsed = Candle {
             open,
             close,
             high,
@@ -72,6 +76,7 @@ fn transform_kline_data(data: String) -> Vec<BinanceKline> {
     return result;
 }
 
+
 #[tokio::main]
 pub async fn main() {
     let temp_file = "data/temp.zip";
@@ -79,7 +84,17 @@ pub async fn main() {
         .await
         .unwrap();
     let raw_data = unpack_binance_kline_data(temp_file);
-    let data = transform_kline_data(raw_data);
+    let mut data = transform_kline_data(raw_data).into_iter();
 
-    println!("get data count {}", data.len());
+    let mut macd = MACD::default();
+    macd.period3 = 4; // setting signal period MA to 4
+    macd.method1 = "sma".parse().unwrap(); // one way of defining methods inside indicators
+    macd.method3 = RegularMethods::TEMA; // another way of defining methods inside indicators
+    let mut macd = macd.init(&data.next().unwrap()).unwrap();
+
+    for candle in data.take(10) {
+        let result = macd.next(&candle);
+    
+        println!("{:?}", result);
+    }
 }
