fb7fec7ce59119675a0adb88e454cad0d0242b84
Cyrus Tang <cyrus.tang@superoverflow.com>
2021-11-16 14:21:55 +0800

    implement account

--- src/account.rs
+++ src/account.rs
@@ -1,4 +1,4 @@
-use chrono::NaiveDateTime;
+use chrono::{NaiveDate, NaiveDateTime};
 
 struct Account {
     available_fund: f64,
@@ -9,8 +9,9 @@ struct Account {
 
 #[derive(Debug)]
 struct TimeValue {
-    time_stamp: NaiveDateTime,
-    value: f64,
+    timestamp: NaiveDateTime,
+    realised_pnl: f64,
+    unrealised_pnl: f64,
 }
 
 #[derive(Debug, PartialEq)]
@@ -19,39 +20,83 @@ struct Position {
     cost: f64,
 }
 
+#[derive(Debug, PartialEq)]
 struct Trade {
-    trade_time: NaiveDateTime,
+    timestamp: NaiveDateTime,
     buy_sell_indicator: BuySellIndicator,
     quantity: f64,
     price: f64,
+    fee: f64,
 }
 
+#[derive(Debug, PartialEq)]
 enum BuySellIndicator {
     Buy,
     Sell,
 }
 
 impl Account {
-    fn new(fund: f64, initial_position: Position) -> Account {
+    fn new(fund: f64, initial_position: Position, start_timestamp: NaiveDateTime) -> Account {
+        let initial_pnl = TimeValue {
+            timestamp: start_timestamp,
+            realised_pnl: 0.,
+            unrealised_pnl: 0.,
+        };
         Account {
             available_fund: fund,
             position: initial_position,
-            profit_and_loss_history: Vec::new(),
+            profit_and_loss_history: vec![initial_pnl],
             trade_history: Vec::new(),
         }
     }
 
-    fn open(&mut self, quantity: f64, price: f64, fee: f64) {
+    fn open(&mut self, timestamp: NaiveDateTime, quantity: f64, price: f64, fee: f64) {
         self.position.cost = (self.position.quantity * self.position.cost + quantity * price)
             / (self.position.quantity + quantity);
         self.position.quantity += quantity;
         self.available_fund -= fee;
+
+        self.trade_history.push(Trade {
+            timestamp,
+            buy_sell_indicator: BuySellIndicator::Buy,
+            quantity,
+            price,
+            fee,
+        });
     }
 
-    fn close(&mut self, quantity: f64, price: f64, fee: f64) {
+    fn close(&mut self, timestamp: NaiveDateTime, quantity: f64, price: f64, fee: f64) {
+        // book profit/loss
+        let last_pnl = self.profit_and_loss_history.last().unwrap();
+        let realised_pnl = quantity * (price - self.position.cost);
+        let current_pnl = TimeValue {
+            realised_pnl,
+            ..*last_pnl
+        };
+        self.profit_and_loss_history.push(current_pnl);
+
         self.position.quantity -= quantity;
         self.available_fund += quantity * price;
         self.available_fund -= fee;
+
+        self.trade_history.push(Trade {
+            timestamp,
+            buy_sell_indicator: BuySellIndicator::Sell,
+            quantity,
+            price,
+            fee,
+        });
+    }
+
+    fn mark_to_market(&mut self, closing_price: f64, timestamp: NaiveDateTime) {
+        let last_pnl = self.profit_and_loss_history.last().unwrap();
+        let unrealised_pnl = self.position.quantity * (closing_price - self.position.cost);
+        let current_pnl = TimeValue {
+            timestamp,
+            unrealised_pnl,
+            realised_pnl: last_pnl.realised_pnl,
+        };
+        self.profit_and_loss_history.push(current_pnl);
     }
 }
 
@@ -65,7 +110,8 @@ mod tests {
             quantity: 123.1,
             cost: 10.0,
         };
-        let account = Account::new(1000.0, initial_position);
+        let start_timestamp = NaiveDate::from_ymd(2021, 9, 1).and_hms(0, 0, 0);
+        let account = Account::new(1000.0, initial_position, start_timestamp);
         assert_eq!(account.position.quantity, 123.1);
     }
 
@@ -75,8 +121,10 @@ mod tests {
             quantity: 100.0,
             cost: 10.0,
         };
-        let mut account = Account::new(5000.0, initial_position);
-        account.open(100.0, 20.0, 0.02);
+        let start_timestamp = NaiveDate::from_ymd(2021, 9, 1).and_hms(0, 0, 0);
+        let mut account = Account::new(5000.0, initial_position, start_timestamp);
+        let timestamp = NaiveDate::from_ymd(2021, 10, 31).and_hms(0, 0, 0);
+        account.open(timestamp, 100.0, 20.0, 0.02);
         assert_eq!(
             account.position,
             Position {
@@ -84,7 +132,15 @@ mod tests {
                 quantity: 200.0
             }
         );
-        assert_eq!(4999.98, account.available_fund)
+        assert_eq!(4999.98, account.available_fund);
+        assert_eq!(vec![Trade { 
+            timestamp: NaiveDate::from_ymd(2021, 10, 31).and_hms(0, 0, 0),
+            buy_sell_indicator: BuySellIndicator::Buy,
+            quantity: 100.0,
+            price: 20.0,
+            fee: 0.02,
+        }], account.trade_history)
+        
     }
 
     #[test]
@@ -93,8 +149,10 @@ mod tests {
             quantity: 100.0,
             cost: 10.0,
         };
-        let mut account = Account::new(1000.0, initial_position);
-        account.close(50.0, 20.0, 0.02);
+        let start_timestamp = NaiveDate::from_ymd(2021, 9, 1).and_hms(0, 0, 0);
+        let mut account = Account::new(1000.0, initial_position, start_timestamp);
+        let timestamp = NaiveDate::from_ymd(2021, 10, 31).and_hms(0, 0, 0);
+        account.close(timestamp, 50.0, 20.0, 0.02);
         assert_eq!(
             account.position,
             Position {
@@ -103,5 +161,12 @@ mod tests {
             }
         );
         assert_eq!(account.available_fund, 1999.98);
+        assert_eq!(vec![Trade { 
+            timestamp: NaiveDate::from_ymd(2021, 10, 31).and_hms(0, 0, 0),
+            buy_sell_indicator: BuySellIndicator::Sell,
+            quantity: 50.0,
+            price: 20.0,
+            fee: 0.02,
+        }], account.trade_history)
     }
 }
